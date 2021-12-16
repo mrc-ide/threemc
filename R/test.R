913e8d315912ab2ce7745f0c26153026bc298c00
paddy7wb <paddy7wb@gmail.com>
2021-12-16 15:39:39 +0000

    first commit

--- R/test.R
+++ R/test.R
@@ -1,5 +1,13 @@
+##' @title Test function for addition
+#' 
+#' @description test
+#' 
+#' @param x value 1
+#' @param y value 2
+#' 
+#' @return sum of x and y
 #' @export
 # test function for R
 sum <- function(x, y) {
-    return(x, y)
+    return(sum(x, y))
 }
