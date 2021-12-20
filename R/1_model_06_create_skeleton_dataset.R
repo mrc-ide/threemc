514c4d6f3deb04e9cea25db9e1b7848778c59bf2
paddy7wb <paddy7wb@gmail.com>
2021-12-20 17:47:53 +0000

    function to create skeleton dataset, which includes the empirical number of person years and numbers for each type of circumcision required for survival analysis by age and time. Help file is wrong atm! Use of sapply and multiple assignment results in significantly less code than was seen in analysis for same calculation

---
+++ R/1_model_06_create_skeleton_dataset.R
@@ -0,0 +1,97 @@
+#' @title Create Skeleton Dataset for Estimating Empirical Circumcision Rate
+#'
+#' @description Create a matrix to estimate the cumulative hazard rate needed
+#' for survival analysis by age and time. The option to include an additional 
+#' stratification variable is also available, creating a 3D hazard function.
+#' 
+#' @param dat Dataset used for modelling.
+#' @param subset Subset for dataset.
+#' @param time1 Variable name for time of birth.
+#' @param time2 Variable name for time circumcised or censored.
+#' @param timecaps Window to fix temporal dimension before and after.
+#' @param Ntime Number of time points (if NULL, function will calculate).
+#' @param age - Variable with age circumcisied or censored.
+#' @param Nage Number of age groups (if NULL, function will calculate).
+#' @param strat Variable to stratify by in using a 3D hazard function.
+#' @param Nstrat Number of stratification groups (if NULL, function will 
+#' calculate).
+#' 
+#' @return Matrix for selecting instananeous hazard rate.
+#' @export
+#' 
+#' @import dplyr
+#' @importFrom tidyr crossing
+
+# function to create skeleton dataset for estimating the empirical circ rate
+
+# survey_circumcision_test <- survey_circumcision
+survey_circumcision <- survey_circumcision_test
+
+create_skeleton_dataset <- function(survey_circumcision, 
+                                    areas, 
+                                    area_lev,
+                                    time1  = "time1",
+                                    time2  = "time2",
+                                    strat  = "space",
+                                    age    = "age",
+                                    circ   = "indweight_st") {
+  if (missing(area_lev)) {
+    warning("area_lev arg missing, taken as maximum area level in areas")
+    area_lev <- max(areas$area_level, na.rm = T)
+  }
+  if ("sf" %in% class(areas)) {
+    areas_model <- st_drop_geometry(areas)
+  } else areas_model <- areas
+  areas_model <- areas_model %>%
+    filter(area_level == area_lev) %>%
+    select(area_id, area_name, space)
+  
+  #' create skeleton dataset with row for every unique area_id, area_name, 
+  #' space, year and circ_age
+  out <- crossing(areas_model,
+                  year = seq(2006, 2021, by =  1),
+                  circ_age = 0 : max(survey_circumcision$circ_age)) %>%
+    # Getting time and age variable
+    mutate(time = year - 2006 + 1,
+           age  = circ_age + 1) %>%
+    # Sorting dataset
+    arrange(space, age, time)
+  
+  # Obtain N person years (via calculating agetime integration matrix)
+  out_int_mat <- create_integration_matrix_agetime(
+    dat = survey_circumcision,
+    time1 = time1,
+    time2 = time2,
+    strat = strat,
+    age   = age,
+    Ntime = length(unique(out$time))
+  )
+  out$N <- as.vector(survey_circumcision$indweight_st %*% out_int_mat)
+  
+  # calculate empirical agetime hazard matrices for different circumcision:
+  subsets <- c("event == 1 & type == 'MMC'", # N MMC 
+               "event == 1 & type == 'TMC'", # N TMC
+               "event == 0", # N censored (i.e. not circumcised)
+               "event == 2" # N left-censored (circ at unknown age)
+  )
+  
+  agetime_hazard_matrices <- lapply(subsets, function(x) {
+    create_hazard_matrix_agetime(
+      dat = survey_circumcision,
+      subset = x,
+      time1 = time1,
+      time2 = time2,
+      strat = strat,
+      age   = age,
+      circ  = circ,
+      Ntime = length(unique(out$time))
+    ) %>%
+      colSums()
+  })
+  
+  # add to out:
+  empirical_circ_cols <- c("obs_mmc", "obs_tmc", "cens", "icens")
+  out[, empirical_circ_cols] <- agetime_hazard_matrices
+  
+  return(out)
+}
