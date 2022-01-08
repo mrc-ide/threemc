57ec2299e0745d101dba8dfabacb5b2a59f35d5c
paddy7wb <paddy7wb@gmail.com>
2022-01-08 16:14:45 +0000

    Feed ... arguments into data loading functions

--- R/read_circ_data.R
+++ R/read_circ_data.R
@@ -1,34 +1,35 @@
 #' @title Function to read in Circumcision Data
-#' 
-#' @description Function to read in circumcision data to fit model. Handles 
+#'
+#' @description Function to read in circumcision data to fit model. Handles
 #' csv with \code{\link[data.table]{fread}} (but outputs data as a
 #' `data.frame`), and geographical data with code{\link[sf]{read_sf}} (for which
 #' it also adds unique identifiers for each `area_level`).
-#' 
+#'
 #' @param path Path to data.
-#' @param filters Optional named vector, whose values dictate the values 
-#' filtered for in the corresponding column names. Only supports filtering for 
+#' @param filters Optional named vector, whose values dictate the values
+#' filtered for in the corresponding column names. Only supports filtering for
 #' one value for each column. default: NULL
-#' 
-#' @seealso 
-#'  \code{\link[data.table]{fread}} 
-#'  \code{\link[sf]{read_sf}} 
-#' @return relevant data set, filtered as desired. 
+#' @param ... Further arguments passed to or from other methods.
+#'
+#' @seealso
+#'  \code{\link[data.table]{fread}}
+#'  \code{\link[sf]{read_sf}}
+#' @return relevant data set, filtered as desired.
 #' @export
 #'
 #' @import dplyr
 #' @import sf
 #' @import rlang
-read_circ_data <- function(path, filters = NULL) {
-  
-  ## maybe add a warning for missing "circ" columns for surveys?? And add them in
-  ## NAs in this situation (look at KEN for this)
+read_circ_data <- function(path, filters = NULL, ...) {
+
+  ## maybe add a warning for missing "circ" columns for surveys?? And add
+  ## in NAs in this situation (look at KEN for this)
 
   ## read in data, depending on file type
-  if (tools::file_ext(path) == "geojson") {
-    .data <- read_sf(path)
-  } else .data <- as.data.frame(data.table::fread(path))
-  
+  if (tools::file_ext(path)%in% c("geojson", "shp", "shx")) {
+    .data <- read_sf(path, ...)
+  } else .data <- as.data.frame(data.table::fread(path, ...))
+
   ## if desired, recursively filter data with provided `filters` vector
   if (!is.null(filters)) {
     cols <- names(filters)
