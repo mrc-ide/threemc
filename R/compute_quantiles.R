7ec1e1582748c2636c275049730703baeb675e54
paddy7wb <paddy7wb@gmail.com>
2022-02-10 14:51:39 +0000

    compute_quantiles had Mean, Lower and Upper error bounds in incorrect order

--- R/compute_quantiles.R
+++ R/compute_quantiles.R
@@ -29,43 +29,49 @@
 ## maybe add "textProgressBar" to for loop if it's too slow?
 compute_quantiles <- function(out,
                               fit,
-                              probs = c(0.5, 0.025, 0.975),
+                              probs = c(0.025, 0.5, 0.975),
                               names = FALSE, ...) {
-  probs <- sort(probs)
 
-  ## function to do "legwork" of computing quantiles
-  quantile_fun <- function(.data, probs = probs, names = names, ...) {
-    .data <- t(apply(.data, 1, stats::quantile,
-      probs = probs, names = names, ...
-    ))
-  }
+    if (length(probs) != 3) stop("probs should be of length 3")
+    # ensure that probs are sorted, regardless of input order
+    probs <- sort(probs)
 
-  ## want to add quantile columns to out for following hazards
-  types <- c(
-    "rate_mmc", "rate_tmc", "rate", "surv", # rates
-    "inc_tmc", "inc_mmc", "inc", # incidence
-    "cum_inc_tmc", "cum_inc_mmc", "cum_inc"
-  ) # cumulative incidence
-  ## append "M" (median), "L" (lower) and "U" (upper) to these column names
-  types <- lapply(types, function(x) paste0(rep(x, 3), c("M", "L", "U")))
+    ## function to do "legwork" of computing quantiles
+    quantile_fun <- function(.data, probs = probs, names = names, ...) {
+        .data <- t(apply(.data, 1, stats::quantile,
+                         probs = probs, names = names, ...
+        ))
+    }
 
-  ## pull samples for each of these hazards from fit object
-  samples <- with(
-    fit$sample,
-    list(
-      haz_mmc, haz_tmc, haz_mmc + haz_tmc, surv,
-      inc_tmc, inc_mmc, inc_tmc + inc_mmc,
-      cum_inc_tmc, cum_inc_mmc, cum_inc_tmc + cum_inc_mmc
+    ## want to add quantile columns to out for following hazards
+    types <- c(
+        "rate_mmc", "rate_tmc", "rate", "surv", # rates
+        "inc_mmc", "inc_tmc", "inc", # incidence
+        "cum_inc_mmc", "cum_inc_tmc", "cum_inc"
+    ) # cumulative incidence
+    # append "L" (lower), "M" (mean), and "U" (upper) to these column names
+    # probs must be sorted to ensure this is in the right order
+    types <- lapply(types, function(x) paste0(rep(x, 3), c("L", "M", "U")))
+
+
+    ## pull samples for each of these hazards from fit object
+    samples <- with(
+        fit$sample,
+        list(
+            haz_mmc, haz_tmc, haz,
+            surv,
+            inc_mmc, inc_tmc, inc_mc,
+            cum_inc_mmc, cum_inc_tmc, cum_inc_mc
+        )
     )
-  )
-  ## for each hazard, calculate quantiles and add as appropriate cols to out
-  for (i in seq_along(types)) {
-    out[, c(types[[i]])] <- quantile_fun(samples[[i]],
-      probs = probs,
-      names = names,
-      ...
-    )
-  }
+    ## for each hazard, calculate quantiles and add as appropriate cols to out
+    for (i in seq_along(types)) {
+        out[, c(types[[i]])] <- quantile_fun(samples[[i]],
+                                             probs = probs,
+                                             names = names,
+                                             ...
+        )
+    }
 
-  return(out)
+    return(out)
 }
