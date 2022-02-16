709a7236840a327f983fa9c953e871611b81a00c
paddy7wb <paddy7wb@gmail.com>
2022-02-16 17:40:47 +0000

    avoid nesting loop if possible

--- R/create_dirs_r.R
+++ R/create_dirs_r.R
@@ -13,17 +13,16 @@ create_dirs_r <- function(dir_path) {
   if (!cond) {
     dirs <- dirs[-length(dirs)] # remove file name
   }
-  if (length(dirs > 0)) {
-    # loop through each directory level in target directory, create any missing
-    for (i in seq_along(dirs)) {
-      # "recursively" check directories
-      if (i == 1) {
-        spec_dir <- dirs[i]
-      } else spec_dir <- paste(dirs[1:i], collapse = "/")
-      # create if missing
-      if (!dir.exists(spec_dir)) {
-        dir.create(spec_dir)
-      }
+  if (length(dirs) == 0) stop("No missing directories created") 
+  # loop through each directory level in target directory, create any missing
+  for (i in seq_along(dirs)) {
+    # "recursively" check directories
+    if (i == 1) {
+      spec_dir <- dirs[i]
+    } else spec_dir <- paste(dirs[1:i], collapse = "/")
+    # create if missing
+    if (!dir.exists(spec_dir)) {
+      dir.create(spec_dir)
     }
   }
 }
