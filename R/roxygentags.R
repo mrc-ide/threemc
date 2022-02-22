fb248e4362bb6855731adcb50f5acd5098240bdf
paddy7wb <paddy7wb@gmail.com>
2022-02-22 13:12:56 +0000

    Add c++ file to documentation and namespace

---
+++ R/roxygentags.R
@@ -0,0 +1,10 @@
+#' Roxygen commands
+#'
+#' This is a dummy function who's purpose is to hold the useDynLib roxygen tag.
+#' This tag will populate the namespace with compiled c++ functions upon package install.
+#'
+#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType.cpp
+#'
+dummy <- function(){
+    return(NULL)
+}
