d183982c9e26cb0b6ea19dca00c0e1066626ecf2
paddy7wb <paddy7wb@gmail.com>
2022-02-22 13:47:01 +0000

    no need for "dummy" function, can just use NULL

--- R/roxygentags.R
+++ R/roxygentags.R
@@ -3,8 +3,6 @@
 #' This is a dummy function who's purpose is to hold the useDynLib roxygen tag.
 #' This tag will populate the namespace with compiled c++ functions upon package install.
 #'
-#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType.cpp
+#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType
 #'
-dummy <- function(){
-    return(NULL)
-}
+NULL
