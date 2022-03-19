c408373785d48164e797725787754af7feaa4ea9
Matthew Thomas <19608881+matthewllthomas@users.noreply.github.com>
2022-03-19 14:20:25 +0000

    Major updates to function
    The function was first updated so that the two cases where we have and don't have a stratification variable are dealt with the same piece of code. Done by introducing a dummy variable when no stratification variables specified.
    
    The secondly the function now considers two cases when we want to run (a) on all areas and (b) aggregated from the reference area level. The former is needed to calculate the PYRS and the latter is needed for the likelihood of the function.

--- R/create_hazard_matrix_agetime.R
+++ R/create_hazard_matrix_agetime.R
@@ -31,6 +31,8 @@
 #' @rdname create_hazard_matrix_agetime
 #' @export
 create_hazard_matrix_agetime <- function(dat,
+                                         areas, 
+                                         area_lev, 
                                          subset = NULL,
                                          time1 = "time1",
                                          time2 = "time2",
@@ -40,55 +42,116 @@ create_hazard_matrix_agetime <- function(dat,
                                          Nage = NULL,
                                          strat = NULL,
                                          Nstrat = NULL,
-                                         circ = "circ") {
-
+                                         circ = "circ",
+                                         aggregated = FALSE,
+                                         weight = NULL) {
+  
   ## Integration matrix for cumulative hazard
   dat$time1_cap <- pmin(
     timecaps[2] - timecaps[1] + 1,
     pmax(1, as.numeric(dat[[time1]]) - timecaps[1] + 1)
   )
-
+  
   ## Integration matrix for cumulative hazard
   dat$time2_cap <- pmin(
     timecaps[2] - timecaps[1] + 1,
     pmax(1, as.numeric(dat[[time2]]) - timecaps[1] + 1)
   )
-
+  
+  ## If no stratification variable create a dummy variable
+  if (is.null(strat)){
+    strat <- 'strat'
+    dat$strat <- 1
+  }
+  
   ## Number of dimensions in the hazard function
   if (is.null(Ntime)) Ntime <- max(dat[, "time1_cap", drop = TRUE])
   if (is.null(Nage)) Nage <- max(dat[age])
-  if (!is.null(strat) & is.null(Nstrat)) Nstrat <- max(dat[strat])
-
+  if (is.null(Nstrat)) Nstrat <- max(dat[strat])
+  
   ## Subsetting data if necessary
   if (!is.null(subset)) {
     dat <- subset(dat, eval(parse(text = subset)))
   }
-  ## Matrix for 2D age time hazard function if strat is NULL
-  if (is.null(strat)) {
-    cols <- unlist(apply(dat, 1, function(x) {
-      Ntime * (as.numeric(x[age]) - 1) + as.numeric(x["time2_cap"])
-    }, simplify = FALSE))
-
-    ## Matrix dimension
-    ncol <- Ntime * Nage
+  
+  # If the selection matrices need to be taken from one reference aggregation 
+  # then we get a list of the hierarchical structure to that level
+  if (aggregated == TRUE){
+    # Adding row identifier for matrix creation
+    dat$row <- 1:nrow(dat)
+    
+    ## If no stratification variable create a dummy variable
+    if (is.null(weight)){
+      weight <- 'weight'
+      dat$weight <- 1
+    }
+    
+    # Getting aggregation structure
+    areas_agg <- create_aggregate_structure(areas    = areas,
+                                            area_lev = 5)
+    
+    # Merging on number of times to 
+    # replicate to the main dataset
+    dat <- dat %>%
+      left_join(areas_agg$areas_agg2,
+                by = 'area_id')
+    
+    # Minimum space ID within the reference level
+    min_ref_space <- min(out %>%
+                           dplyr::filter(area_level == area_lev) %>%
+                           dplyr::pull(space))
+    
+    # Only keeping stratums where we have data 
+    dat2 <- subset(dat, eval(parse(text = paste(circ, ' != 0', sep = ''))))
+    
+    # Aggregation for each row in the dataframe
+    entries <- apply(dat2, 1, function(x) {
+      # Getting areas in reference administrative 
+      # boundaries to aggregate over
+      tmp_space <- areas_agg$areas_agg1[[as.numeric(x[strat])]]
+      # Getting columns with non-zero entries for sparse matrix
+      cols <- Ntime * Nage * (tmp_space - min_ref_space) +
+        Ntime * (as.numeric(x[age]) - 1) +
+        as.numeric(x["time2_cap"])
+      print(summary(cols))
+      # Getting rows for sparse matrix
+      rows <- rep(as.numeric(x["row"]), length(cols))
+      # Getting weights 
+      vals <- as.numeric(x[circ]) * dat[cols, weight, drop = TRUE] / sum(dat[cols, weight, drop = TRUE])
+      # Output dataset 
+      tmp <- data.frame(cols, rows, vals)
+      # Return dataframe
+      return(tmp)
+    })
+    
+    # Extracting entries for sparseMatrix
+    cols <- as.numeric(unlist(lapply(entries, "[", "cols")))
+    rows <- as.numeric(unlist(lapply(entries, "[", "rows")))
+    vals <- as.numeric(unlist(lapply(entries, "[", "vals")))
   }
-  ## Matrix for 3D hazard function if strat not NULL
-  if (!is.null(strat)) {
-
-    ## Integration matrix for cumulative hazard
-    cols <- unlist(apply(dat, 1, function(x) {
+  # Else the selection matrices will be taken from the aggregation they are on
+  else{
+    # Only keeping stratums where we have data 
+    dat2 <- subset(dat, eval(parse(text = paste(circ, ' != 0', sep = ''))))
+    
+    ## Column entries for hazard matrix
+    cols <- unlist(apply(dat2, 1, function(x) {
       Ntime * Nage * (as.numeric(x[strat]) - 1) + Ntime *
         (as.numeric(x[age]) - 1) + as.numeric(x["time2_cap"])
     }, simplify = FALSE))
-    ncol <- Ntime * Nage * Nstrat
+    rows <- seq_len(nrow(dat2))
+    vals <- dat2[[circ]]
   }
-  ## Outputting sparse matrix
+  
+  ## Outputting sparse hazard matrix which selects the
+  ## corresponding incidence rates for the likelihood.
   A <- Matrix::sparseMatrix(
-    i = seq_len(nrow(dat)),
+    i = rows,
     j = cols,
-    x = dat[[circ]],
-    dims = c(nrow(dat), ncol)
+    x = vals,
+    dims = c(nrow(dat2), Ntime * Nage * Nstrat)
   )
+  
   ## Returning matrix
   return(A)
 }
