d8a700bf4d3351883db64208d695fef0a46cf9a3
Matthew Thomas <19608881+matthewllthomas@users.noreply.github.com>
2022-03-19 14:13:38 +0000

    Updating the integration matrices to remove separate case for no strat
    Updated both integration matrices functions so that the two cases where we have and don't have a stratification variable are dealt with the same piece of code. Done by introducing a dummy variable when no stratification variables specified.

--- R/create_integration_matrix_agetime.R
+++ R/create_integration_matrix_agetime.R
@@ -35,109 +35,80 @@ create_integration_matrix_agetime <- function(dat,
                                               Nage = NULL,
                                               strat = NULL,
                                               Nstrat = NULL) {
-
-
+  
+  
   # !! JE: Matt -- check these lines; I think this can be done with
   # pmin()/pmax() and does not need an unlist() because it will always return
   # a vector.
-
+  
   # Integration matrix for cumulative hazard
   dat$time1_cap <- pmin(
     timecaps[2] - timecaps[1],
     pmax(1, as.numeric(dat[[time1]]) - timecaps[1] + 1)
   )
-
+  
   # Integration matrix for cumulative hazard
   dat$time2_cap <- pmin(
     timecaps[2] - timecaps[1] + 1,
     pmax(1, as.numeric(dat[[time2]]) - timecaps[1] + 1)
   )
-
+  
   # Shifting time points by the time caps
   dat$time1_cap2 <- dat[[time1]] - timecaps[1] + 1
   dat$time2_cap2 <- dat[[time2]] - timecaps[1] + 1
-
+  
+  ## If no stratification variable create a dummy variable
+  if (is.null(strat)){
+    strat <- 'strat'
+    dat$strat <- 1
+  }
+  
   # Number of dimensions in the hazard function
   if (is.null(Ntime)) Ntime <- max(dat[["time1_cap"]])
   if (is.null(Nage)) Nage <- max(dat[age])
   if (is.null(strat) == FALSE & is.null(Nstrat)) Nstrat <- max(dat[strat])
-
+  
   # Subsetting data if necessary
   if (is.null(subset) == FALSE) {
     dat <- subset(dat, eval(parse(text = subset)))
   }
   # Adding dummy variable for the rows of the matrix
   dat$row <- seq_len(nrow(dat))
-
-  # Matrix for 3D hazard function if strat not NULL
-  if (is.null(strat)) {
-
-    # !! JE: Matt -- could this condition be combined with the next one
-    #        by setting Nstrat = 1 if is.null(strat) = TRUE?
-
-    # column entries for integration matrix
-    cols <- unlist(apply(dat, 1, function(x) {
-      # If circumcised at birth select relevant entry
-      if (as.numeric(x["time1_cap2"]) == (as.numeric(x["time2_cap2"]))) {
+  
+  # column entries for integration matrix
+  cols <- unlist(apply(dat, 1, function(x) {
+    # If circumcised at birth select relevant entry
+    if (as.numeric(x["time1_cap2"]) == (as.numeric(x["time2_cap2"]))) {
+      Ntime * Nage * (as.numeric(x[strat]) - 1) +
         min(
           timecaps[2] - timecaps[1] + 1,
           max(1, as.numeric(x["time1_cap2"]))
         )
-        # Else just estimate the ??
-      } else {
-        cumsum(
-          c(
+    } else {
+      # Else just estimate the
+      cumsum(
+        c(
+          Ntime * Nage * (as.numeric(x[strat]) - 1) +
             max(1, as.numeric(x["time1_cap2"])),
-            Ntime + (as.numeric(x["time1_cap2"]):
-            (as.numeric(x["time2_cap2"]) - 1) > 0 &
-              as.numeric(x["time1_cap2"]):
-              (as.numeric(x["time2_cap2"]) - 1) <=
-                timecaps[2] - timecaps[1])
-          )
+          Ntime + (as.numeric(x["time1_cap2"]):
+                     (as.numeric(x["time2_cap2"]) - 1) > 0 &
+                     as.numeric(x["time1_cap2"]):
+                     (as.numeric(x["time2_cap2"]) - 1) <=
+                     timecaps[2] - timecaps[1])
         )
-      }
-    }, simplify = FALSE))
-
-    # Matrix dimension
-    ncol <- Ntime * Nage
-  }
-  # Matrix for 3D hazard function if strat not NULL
-  if (!is.null(strat)) {
-
-    # column entries for integration matrix
-    cols <- unlist(apply(dat, 1, function(x) {
-      # If circumcised at birth select relevant entry
-      if (as.numeric(x["time1_cap2"]) == (as.numeric(x["time2_cap2"]))) {
-        Ntime * Nage * (as.numeric(x[strat]) - 1) +
-          min(
-            timecaps[2] - timecaps[1] + 1,
-            max(1, as.numeric(x["time1_cap2"]))
-          )
-      } else {
-        # Else just estimate the
-        cumsum(
-          c(
-            Ntime * Nage * (as.numeric(x[strat]) - 1) +
-              max(1, as.numeric(x["time1_cap2"])),
-            Ntime + (as.numeric(x["time1_cap2"]):
-            (as.numeric(x["time2_cap2"]) - 1) > 0 &
-              as.numeric(x["time1_cap2"]):
-              (as.numeric(x["time2_cap2"]) - 1) <=
-                timecaps[2] - timecaps[1])
-          )
-        )
-      }
-    }, simplify = FALSE))
-
-    # Matrix dimension
-    ncol <- Ntime * Nage * Nstrat
-  }
-
+      )
+    }
+  }, simplify = FALSE))
+  
+  # Matrix dimension
+  ncol <- Ntime * Nage * Nstrat
+  
+  
   # Row entries for integration matrix
   rows <- unlist(apply(dat, 1, function(x) {
     rep(as.numeric(x["row"]), as.numeric(x[time2]) - as.numeric(x[time1]) + 1)
   }, simplify = FALSE))
-
+  
   # Outputting sparse matrix
   A <- Matrix::sparseMatrix(
     i = rows,
--- R/create_integration_matrix_agetime_lag.R
+++ R/create_integration_matrix_agetime_lag.R
@@ -47,11 +47,17 @@ create_integration_matrix_agetime_lag <- function(dat,
     timecaps[2] - timecaps[1] + 1,
     pmax(1, as.numeric(dat[[time2]]) - timecaps[1] + 1)
   )
-
+  
   # Shifting time points by the time caps
   dat$time1_cap2 <- dat[[time1]] - timecaps[1] + 1
   dat$time2_cap2 <- dat[[time2]] - timecaps[1] + 1
-
+  
+  ## If no stratification variable create a dummy variable
+  if (is.null(strat)){
+    strat <- 'strat'
+    dat$strat <- 1
+  }
+  
   # Number of dimensions in the hazard function
   if (is.null(Ntime)) Ntime <- max(dat[, "time1_cap", drop = TRUE])
   if (is.null(Nage)) Nage <- max(dat[age])
@@ -62,74 +68,45 @@ create_integration_matrix_agetime_lag <- function(dat,
   }
   # Number of rows in the resulting matrix
   nrow <- nrow(dat)
+               
   # Adding dummy variable for the rows of the matrix
   dat$row <- seq_len(nrow(dat))
-
-  # Matrix for 3D hazard function if strat not NULL
-  if (is.null(strat)) {
-
-    # column entries for integration matrix
-    cols <- unlist(apply(dat, 1, function(x) {
-      # If circumcised at birth select relevant entry
-      if (as.numeric(x["time1_cap2"]) == (as.numeric(x["time2_cap2"]))) {
-        test <- min(
+  
+  # column entries for integration matrix
+  cols <- unlist(apply(dat, 1, FUN = function(x) {
+    # If circumcised at birth select relevant entry
+    if (as.numeric(x["time1_cap2"]) == (as.numeric(x["time2_cap2"]))) {
+      test <- Ntime * Nage * (as.numeric(x[strat]) - 1) +
+        min(
           timecaps[2] - timecaps[1] + 1,
           max(1, as.numeric(x["time1_cap2"]))
         )
-      } else {
-        # Else just estimate the
-        test <- cumsum(
-          c(
+    } else {
+      # Else just estimate the
+      test <- cumsum(
+        c(
+          Ntime * Nage * (as.numeric(x[strat]) - 1) +
             max(1, as.numeric(x["time1_cap2"])),
-            Ntime + (as.numeric(x["time1_cap2"]):
-            (as.numeric(x["time2_cap2"]) - 1) > 0 &
-              as.numeric(x["time1_cap2"]):
-              (as.numeric(x["time2_cap2"]) - 1) <=
-                timecaps[2] - timecaps[1])
-          )
+          Ntime + (as.numeric(x["time1_cap2"]):
+                     (as.numeric(x["time2_cap2"]) - 1) > 0 &
+                     as.numeric(x["time1_cap2"]):
+                     (as.numeric(x["time2_cap2"]) - 1) <=
+                     timecaps[2] - timecaps[1])
         )
-      }
-      test <- test[-length(test)]
-      return(test)
-    }, simplify = FALSE))
-  }
-  # Matrix for 3D hazard function if strat not NULL
-  if (!is.null(strat)) {
-
-    # column entries for integration matrix
-    cols <- unlist(apply(dat, 1, FUN = function(x) {
-      # If circumcised at birth select relevant entry
-      if (as.numeric(x["time1_cap2"]) == (as.numeric(x["time2_cap2"]))) {
-        test <- Ntime * Nage * (as.numeric(x[strat]) - 1) +
-          min(
-            timecaps[2] - timecaps[1] + 1,
-            max(1, as.numeric(x["time1_cap2"]))
-          )
-      } else {
-        # Else just estimate the
-        test <- cumsum(
-          c(
-            Ntime * Nage * (as.numeric(x[strat]) - 1) +
-              max(1, as.numeric(x["time1_cap2"])),
-            Ntime + (as.numeric(x["time1_cap2"]):
-            (as.numeric(x["time2_cap2"]) - 1) > 0 &
-              as.numeric(x["time1_cap2"]):
-              (as.numeric(x["time2_cap2"]) - 1) <=
-                timecaps[2] - timecaps[1])
-          )
-        )
-      }
-      test <- test[-length(test)]
-      return(test)
-    }, simplify = FALSE))
-
-    ncol <- Ntime * Nage * Nstrat
-  }
+      )
+    }
+    test <- test[-length(test)]
+    return(test)
+  }, simplify = FALSE))
+  
+  # Number of columns 
+  ncol <- Ntime * Nage * Nstrat
+  
   # Row entries for integration matrix
   rows <- unlist(apply(dat, 1, function(x) {
     rep(as.numeric(x["row"]), as.numeric(x[time2]) - as.numeric(x[time1]))
   }, simplify = FALSE))
-
+  
   # Outputting sparse matrix
   A <- Matrix::sparseMatrix(
     i = rows,
