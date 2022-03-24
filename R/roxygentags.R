9203cadd4b821d70bdb590a46b38b7b9d24ae247
paddy7wb <paddy7wb@gmail.com>
2022-03-24 17:37:07 +0000

    add title to prevent warning when documenting

--- R/roxygentags.R
+++ R/roxygentags.R
@@ -1,4 +1,4 @@
-#' Roxygen commands
+#' @title Export TMB models
 #'
 #' This is a dummy function who's purpose is to hold the useDynLib roxygen tag.
 #' This tag will populate the namespace with compiled c++ functions upon package install.
