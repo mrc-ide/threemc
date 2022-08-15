c199f598fbd7a452a25a6b81573901c69613194f
paddy7wb <paddy7wb@gmail.com>
2022-08-15 09:36:16 +0100

    Export model with paediatric mmc rate

--- R/roxygentags.R
+++ R/roxygentags.R
@@ -1,14 +1,10 @@
 #' @title Export TMB models
-#'
 #' This is a dummy function who's purpose is to hold the useDynLib roxygen tag.
 #' This tag will populate the namespace with compiled c++ functions upon
 #' package install.
-#'
 #' @useDynLib Surv_SpaceAgeTime
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType
-#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paedeatric_MMC
+#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC
 #' @noRd
 #' @keywords internal
-.dummy <- function() {
-  return(NULL)
-}
+NULL
