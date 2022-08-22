9da903f9f02523e7c1b8f00639a59b2cb2a2fc2d
paddy7wb <paddy7wb@gmail.com>
2022-08-22 14:56:45 +0100

    change population_data to populations to be consistent

--- R/shell_dataset.R
+++ R/shell_dataset.R
@@ -8,7 +8,7 @@
 #'  survey entries (i.e. where circumcision occurred at an unknown age).
 #' @param survey_circumcision Information on male circumcision status from
 #' surveys.
-#' @param population_data Single age male population counts by space and time.
+#' @param populations Single age male population counts by space and time.
 #' @param areas `sf` shapefiles for specific country/region.
 #' @param area_lev  PSNU area level for specific country. Defaults to the
 #' maximum area level found in `areas` if not supplied.
