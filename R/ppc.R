6539971cbc906a8a4e6422657b5c43a7f1f76f3f
paddy7wb <paddy7wb@gmail.com>
2022-09-14 16:45:55 +0100

    Use styler to indent function properly

--- R/ppc.R
+++ R/ppc.R
@@ -35,182 +35,184 @@ threemc_oos_ppc <- function(
     populations,
     survey_estimate,
     removed_years,
-    type     = "coverage",
+    type = "coverage",
     area_lev = 1,
     age_groups = c(
-        # five-year age groups
-        "0-4",   "5-9",   "10-14", "15-19", "20-24", "25-29",
-        "30-34", "35-39", "40-44", "45-49", "50-54", "54-59",
-        # age groups with only minimum cut-off
-        "0+", "10+", "15+",
-        # other, wider age groups of interest
-        "10-24", "15-24", "10-29", "15-29",
-        "10-39", "15-39", "10-49", "15-49"
+      # five-year age groups
+      "0-4", "5-9", "10-14", "15-19", "20-24", "25-29",
+      "30-34", "35-39", "40-44", "45-49", "50-54", "54-59",
+      # age groups with only minimum cut-off
+      "0+", "10+", "15+",
+      # other, wider age groups of interest
+      "10-24", "15-24", "10-29", "15-29",
+      "10-39", "15-39", "10-49", "15-49"
     ),
     CI_range = 0.95,
-    N        = 1000
-) {
+    N = 1000
+  ) {
 
-    #### Join Samples with Results ####
+  #### Join Samples with Results ####
 
-    if (removed_years %in% out$year) {
-        stop(
-            "removed_years in out$year, please reevaulate left-out",
-            " surveys and/or OOS results specification"
-        )
-    }
 
-    # filter results to specified or modelled area level
-    max_area_lev <- max(out$area_level)
-    if (area_lev > max_area_lev) {
-        message(
-            "Specified area lev more granular than data provided, set to ",
-            max_area_lev
-        )
-        area_lev <- max_area_lev
-    }
-    out <- dplyr::filter(out, .data$area_level == area_lev) %>%
-        # join in populations
-        dplyr::left_join(
-            dplyr::select(
-              populations,
-              -c(dplyr::matches("area_name"), dplyr::matches("area_level"))
-            )
-        )
+  if (any(removed_years %in% out$year)) {
+      stop(
+          "removed_years in out$year, please re-evaluate left-out",
+          " surveys and/or OOS results specification"
+      )
+  }
 
-    # pull sample name corresponding with type column values in results
-    sample_colnames <- switch(
-        type,
-        "coverage"    = "cum_inc",
-        "incidence"   = "inc",
-        "probability" = "haz"
+  # filter results to specified or modelled area level
+  max_area_lev <- max(out$area_level)
+  if (area_lev > max_area_lev) {
+    message(
+      "Specified area lev more granular than data provided, set to ",
+      max_area_lev
     )
-    if (is.null(sample_colnames)) {
-        stop(
-            "Please choose a valid type
+    area_lev <- max_area_lev
+  }
+  out <- dplyr::filter(out, .data$area_level == area_lev) %>%
+    # join in populations
+    dplyr::left_join(
+      dplyr::select(
+        populations,
+        -c(dplyr::matches("area_name"), dplyr::matches("area_level"))
+      )
+    )
+
+  # pull sample name corresponding with type column values in results
+  sample_colnames <- switch(type,
+    "coverage"    = "cum_inc",
+    "incidence"   = "inc",
+    "probability" = "haz"
+  )
+  if (is.null(sample_colnames)) {
+    stop(
+      "Please choose a valid type
         (one of 'probability', 'incidence', 'prevalence'"
-        )
-    }
+    )
+  }
 
-    # pull N "cum_inc" samples for each desired output from fit
-    samples <- fit$sample[
-                 grepl(paste0("^", sample_colnames), names(fit$sample))
-               ]
-    # ensure names for MC columns in fit have the suffix "_mc"
-    samples <- append_mc_name(samples)
-    # remove "cum_inc_" and capitalise
-    names(samples) <- paste(
-        toupper(stringr::str_replace_all(names(samples), "cum_inc_", "")), type
-    ) 
+  # pull N "cum_inc" samples for each desired output from fit
+  samples <- fit$sample[
+    grepl(paste0("^", sample_colnames), names(fit$sample))
+  ]
+  # ensure names for MC columns in fit have the suffix "_mc"
+  samples <- append_mc_name(samples)
+  # remove "cum_inc_" and capitalise
+  names(samples) <- paste(
+    toupper(stringr::str_replace_all(names(samples), "cum_inc_", "")), type
+  )
 
 
-    #### Split out between types ####
- 
-    # join coverage col of interest with samples
-    out_types <- lapply(names(samples), function(x) {
-        out_spec <- dplyr::select(out, .data$area_id:.data$population)
-        n <- length(out_spec)
-        out_spec[, (n +1):(n + N)] <- samples[[x]]
-        out_spec <- dplyr::mutate(out_spec, indicator = x)
-    }) %>%
+  #### Split out between types ####
+
+  # join coverage col of interest with samples
+  out_types <- lapply(names(samples), function(x) {
+    out_spec <- dplyr::select(out, .data$area_id:.data$population)
+    n <- length(out_spec)
+    out_spec[, (n + 1):(n + N)] <- samples[[x]]
+    out_spec <- dplyr::mutate(out_spec, indicator = x)
+  }) %>%
     dplyr::bind_rows() %>%
     # only take years where surveys were removed, and modelled area level
     dplyr::filter(.data$year %in% removed_years, .data$area_level == area_lev)
 
 
-    #### Aggregate to Age Groups #### 
-    
-    # change col names to work in aggregate_sample_age_group
-    names(out_types)[grepl("V", names(out_types))] <- paste0("samp_", 1:N)
-    out_types <- dplyr::rename(out_types, type = .data$indicator)
-    
-    out_types_agegroup <- aggregate_sample_age_group(
-        out_types,
-        aggr_cols  = c(
-            "area_id", "area_name", "area_level", "year", "type"
-        ),
-        num_cols   = paste0("samp_", seq_len(N)),
-        age_groups = age_groups
-    ) %>% 
-        # rename to match survey points df
-        dplyr::rename(indicator = .data$type) %>% 
-        dplyr::relocate(
-                   dplyr::contains("samp_"), .after = dplyr::everything()
-        ) %>%
-        # filter for at least area_level 1
-        dplyr::filter(.data$area_level == min(area_lev, 1)) 
-    
-    
-    #### Prepare Survey Points & Join with Samples ####
-    
-    survey_estimate_prep <- survey_estimate %>%
-        # filter for OOS year(s) and modelled area_level
-        dplyr::filter(
-            .data$year       %in% removed_years,
-            .data$area_level == min(area_lev, 1),
-            .data$age_group  %in% out_types_agegroup$age_group,
-            .data$mean       != 0
-        ) %>%
-        # ignore survey_id, merging for the same year
-        dplyr::group_by(
-                   .data$area_id,
-                   .data$year,
-                   .data$age_group,
-                   .data$indicator
-        ) %>%
-        dplyr::summarise(mean = sum(.data$mean), .groups = "drop")
+  #### Aggregate to Age Groups ####
 
-    # join with samples
-    survey_estimate_ppd <- dplyr::left_join(
-                               survey_estimate_prep, out_types_agegroup
-                           )
+  # change col names to work in aggregate_sample_age_group
+  names(out_types)[grepl("V", names(out_types))] <- paste0("samp_", 1:N)
+  out_types <- dplyr::rename(out_types, type = .data$indicator)
 
-    # check for NAs in first sample column
-    stopifnot(!all(is.na(survey_estimate_ppd$samp_1)))
+  out_types_agegroup <- aggregate_sample_age_group(
+    out_types,
+    aggr_cols = c(
+      "area_id", "area_name", "area_level", "year", "type"
+    ),
+    num_cols = paste0("samp_", seq_len(N)),
+    age_groups = age_groups
+  ) %>%
+    # rename to match survey points df
+    dplyr::rename(indicator = .data$type) %>%
+    dplyr::relocate(
+      dplyr::contains("samp_"),
+      .after = dplyr::everything()
+    ) %>%
+    # filter for at least area_level 1
+    dplyr::filter(.data$area_level == min(area_lev, 1))
 
 
-    #### Calculate Posteior Predictive Check for Prevalence Estimations ####
+  #### Prepare Survey Points & Join with Samples ####
 
-    # func calculating where in model sample distn empirical values are
-    quant_pos_sum <- function(y, x) if (y < x) 0 else 1
-   
-    survey_estimate_ppd_dist <- survey_estimate_ppd %>%
-        dplyr::relocate(.data$mean, .before = .data$samp_1) %>%
-        dplyr::group_by(dplyr::across(.data$area_id:.data$area_level)) %>%
-        dplyr::rowwise() %>%
-        dplyr::summarise(
-            quant_pos = sum(
-                dplyr::across(dplyr::starts_with("samp_"), ~ quant_pos_sum(
-                    mean, .x
-            ))),
-            .groups = "drop"
-        )
- 
-    # calculate percentage of samples within 95% CI from PPD
-    x <- survey_estimate_ppd_dist$quant_pos
-    x <- sum(
-        x >= 0.5 * (1 - CI_range) * N &
-        x <= 1 - 0.5 * (1 - CI_range) * N
-    ) / length(x)
-    print(paste0(
-        "Percentage of survey points which fall within posterior predictive",
-        " distribution at ",
-        # 95%
-        CI_range * 100,
-        "%",
-        " CI: ",
-        round(x * 100, 2),
-        "%"
-    ))
- 
-    # TODO: Add option to include additional summary statistics
-    summary_stats <- c(
-        "samples within CI" = x
+  survey_estimate_prep <- survey_estimate %>%
+    # filter for OOS year(s) and modelled area_level
+    dplyr::filter(
+      .data$year %in% removed_years,
+      .data$area_level == min(area_lev, 1),
+      .data$age_group %in% out_types_agegroup$age_group,
+      .data$mean != 0
+    ) %>%
+    # ignore survey_id, merging for the same year
+    dplyr::group_by(
+      .data$area_id,
+      .data$year,
+      .data$age_group,
+      .data$indicator
+    ) %>%
+    dplyr::summarise(mean = sum(.data$mean), .groups = "drop")
+
+  # join with samples
+  survey_estimate_ppd <- dplyr::left_join(
+    survey_estimate_prep, out_types_agegroup
+  )
+
+  # check for NAs in first sample column
+  stopifnot(!all(is.na(survey_estimate_ppd$samp_1)))
+
+
+  #### Calculate Posteior Predictive Check for Prevalence Estimations ####
+
+  # func calculating where in model sample distn empirical values are
+  quant_pos_sum <- function(y, x) if (y < x) 0 else 1
+
+  survey_estimate_ppd_dist <- survey_estimate_ppd %>%
+    dplyr::relocate(.data$mean, .before = .data$samp_1) %>%
+    dplyr::group_by(dplyr::across(.data$area_id:.data$area_level)) %>%
+    dplyr::rowwise() %>%
+    dplyr::summarise(
+      quant_pos = sum(
+        dplyr::across(dplyr::starts_with("samp_"), ~ quant_pos_sum(
+          mean, .x
+        ))
+      ),
+      .groups = "drop"
     )
 
-    return(list(
-        "ppd"           = survey_estimate_ppd,
-        "ppd_dist"      = survey_estimate_ppd_dist,
-        "summary_stats" = summary_stats
-    ))
+  # calculate percentage of samples within 95% CI from PPD
+  x <- survey_estimate_ppd_dist$quant_pos
+  x <- sum(
+    x >= 0.5 * (1 - CI_range) * N &
+      x <= 1 - 0.5 * (1 - CI_range) * N
+  ) / length(x)
+  print(paste0(
+    "Percentage of survey points which fall within posterior predictive",
+    " distribution at ",
+    # 95%
+    CI_range * 100,
+    "%",
+    " CI: ",
+    round(x * 100, 2),
+    "%"
+  ))
+
+  # TODO: Add option to include additional summary statistics
+  summary_stats <- c(
+    "samples within CI" = x
+  )
+
+  return(list(
+    "ppd"           = survey_estimate_ppd,
+    "ppd_dist"      = survey_estimate_ppd_dist,
+    "summary_stats" = summary_stats
+  ))
 }
