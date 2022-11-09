7bc9c3055f02518f625439a9f6e3cc4caa2d6c15
paddy7wb <paddy7wb@gmail.com>
2022-11-09 20:00:29 +0000

    export `threemc.cpp`

--- R/roxygentags.R
+++ R/roxygentags.R
@@ -2,14 +2,7 @@
 #' This is a dummy function who's purpose is to hold the useDynLib roxygen tag.
 #' This tag will populate the namespace with compiled c++ functions upon
 #' package install.
-#' @useDynLib Surv_SpaceAgeTime
-#' @useDynLib Surv_SpaceAgeTime_RW
-#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType
-#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_RW
-#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType2
-#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC
-#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC_RW
-#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC2
+#' @useDynLib threemc, .registration = TRUE
 #' @noRd
 #' @keywords internal
 NULL
