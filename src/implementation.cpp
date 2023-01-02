8964d5cf3f32eb63dc527ab2b56ebbda42f778ff
paddy7wb <paddy7wb@gmail.com>
2023-01-02 16:56:56 +0000

    Move utility functions to implementation file, delete separate header

--- src/implementation.cpp
+++ src/implementation.cpp
@@ -743,3 +743,25 @@ Type threemc(
   /////////////////////////////////////////
   return nll; 
 }
+
+/* Odd Functions */
+
+/***************************************************/
+/* Function to inverse logit transform for vectors */
+/***************************************************/
+template <class vector>
+vector invlogit_vec(vector x){
+  vector y = 1.0 / (1.0 + exp(-x));
+  return y;
+}
+
+/*******************************************************************/
+/* Function to inverse logit transform on a general interval [a,b] */
+/*******************************************************************/
+template <class Type>
+Type geninvlogit(Type x, Type a, Type b){
+  Type y; 
+  y = 1.0 / (1.0 + exp(-x));
+  y = y * (b - a) + a;
+  return y;
+}
