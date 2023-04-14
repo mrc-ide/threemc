f42d600fdc64dd89fbd7a1230fd94acd1bd3783f
paddy7wb <paddy7wb@gmail.com>
2023-04-14 18:24:07 +0100

    Have new models compile on load

--- R/roxygentags.R
+++ R/roxygentags.R
@@ -6,14 +6,20 @@
 #' @useDynLib Surv_SpaceAgeTime_RW
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType2
+#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType3
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_RW
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_RW2
+#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_RW3
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_RW_MMC2
+#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_RW_MMC3
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC2
+#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC3
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC_RW
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC_RW2
+#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC_RW3
 #' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC_RW_MMC2
+#' @useDynLib Surv_SpaceAgeTime_ByType_withUnknownType_Const_Paed_MMC_RW_MMC3
 #' @noRd
 #' @keywords internal
 NULL
