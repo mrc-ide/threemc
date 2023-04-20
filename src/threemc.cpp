a034746446e3eb5ea3c343a80a535612b3573577
paddy7wb <paddy7wb@gmail.com>
2023-04-20 10:44:13 +0100

    Add RW model as option within main cpp file

--- src/threemc.cpp
+++ src/threemc.cpp
@@ -18,17 +18,25 @@ Type objective_function<Type>::operator() ()
   Type nll = Type(0);
 
   // model with type info, no paed age cutoff and no time TMC effect
+  // TODO: Change all of these nested ifs into a switch statement, very ugly!!
   if (threemc_data.is_type == 1) {
-    Threemc<Type> threemc;
+    if (threemc_data.rw_order == 1) {
+      Threemc_rw<Type> threemc;
+      // std::cout << "Message about model selection" << std::endl;
+      threemc.calc_nll(threemc_data, this);
+      nll = threemc.get_nll();
+    } else {
+      Threemc<Type> threemc;
+      // std::cout << "Message about model selection" << std::endl;
+      threemc.calc_nll(threemc_data, this);
+      nll = threemc.get_nll();
+     }
+    // class for model with no type
+  } else if (threemc_data.is_type == 0) {
+    Threemc_nt<Type> threemc;
     // std::cout << "Message about model selection" << std::endl;
     threemc.calc_nll(threemc_data, this);
     nll = threemc.get_nll();
-    // class for model with no type
-  } else if (threemc_data.is_type == 0) {
-    Threemc_nt<Type> threemc_nt;
-    // std::cout << "Message about model selection" << std::endl;
-    threemc_nt.calc_nll(threemc_data, this);
-    nll = threemc_nt.get_nll();
   }
   return nll;
 }
